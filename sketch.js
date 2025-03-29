c79dba90009ac0555d756b982ec0ff6699679940
Soumya Talwar <soumyatalwar@Soumyas-MacBook-Pro.local>
2025-03-30 03:32:54 +0530

    testing backdrop

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
 var user;
 
 $("document").ready(() => {
-  $("#details").hide();
-  $("#start").click(() => {
-    $("#title").fadeOut(300, () => {
-      $("#title, #details").toggleClass("d-none");
-      $("#details").fadeIn(300);
-    });
-  });
+	$("#details").hide();
+	$("#start").click(() => {
+		$("#page-title").fadeOut(300, () => {
+			$("#page-title, #details").toggleClass("d-none");
+			$("#details").fadeIn(300);
+		});
+	});
 
-  $("#submit").click(() => {
-    user = $("#user").val();
-    $("#name").fadeOut(300, () => {
-      $("#name, #route").toggleClass("d-none");
-      $("#route").fadeIn(300);
-    });
-  });
+	$("#submit").click(() => {
+		user = $("#user").val();
+		$("#name").fadeOut(300, () => {
+			$("#name, #route").toggleClass("d-none");
+			$("#route").fadeIn(300);
+		});
+	});
 
-  $("#talk").click(() => {
-    $("#details").fadeOut(300, () => {
-      $("#details, #chat").toggleClass("d-none");
-      $("#chat").fadeIn(300);
-    });
-  });
+	$("#talk").click(() => {
+		$("#details").fadeOut(300, () => {
+			$("#details, #chat").toggleClass("d-none");
+			$("#chat").fadeIn(300);
+		});
+	});
 });
