3e7a750abd4cea5ec1e370fdb3dcba7ec238e582
Soumya Talwar <soumyatalwar@Soumyas-MacBook-Pro.local>
2025-03-30 07:36:59 +0530

    music added

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 var user;
 
 $("document").ready(() => {
+	var music = new Audio("audio/bgm.mp3");
+	$("#play").click(() => {
+		music.play();
+		$("#permission").addClass("d-none");
+		$("#background").css("opacity", "100%");
+	});
 	var info = new Audio("audio/info.m4a");
 	$("#details").hide();
 	$("#info").click(() => info.play());
-	$("#start").click(() => {
-		$("#page-title").fadeOut(300, () => {
-			$("#page-title, #page-details").toggleClass("d-none");
-			$("#page-details").fadeIn(300);
-		});
-	});
+	// $("#start").click(() => {
+	// 	$("#page-title").fadeOut(300, () => {
+	// 		$("#page-title, #page-details").toggleClass("d-none");
+	// 		$("#page-details").fadeIn(300);
+	// 	});
+	// });
 
 	$("#submit").click(() => {
 		user = $("#user").val();
